f442e56c2a15cab31a1de363de2d7174e0b8786c
unknown <edurhoden@gmail.com>
2024-12-18 16:10:06 -0300

    Add code to BIO tag and list the score of predictions

--- bert_trainer/huggingface_pipeline.py
+++ bert_trainer/huggingface_pipeline.py
@@ -1,50 +1,75 @@
 from transformers import pipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForTokenClassification
-
+from transformers import AutoModelForTokenClassification, AutoTokenizer
 
 class Pipeline:
     def __init__(self, checkpoint) -> None:
-        self.tagger = pipeline("ner", model=checkpoint, tokenizer=checkpoint, device=0,)
+        self.tagger = pipeline("ner", 
+                               model=checkpoint, 
+                               tokenizer=AutoTokenizer.from_pretrained(checkpoint, model_max_length=512), 
+                               device=0, # device=0 means using GPU
+                               aggregation_strategy='simple')  
 
-    def split_text(self, text, max_length):
-        tokens = text.split()
-        sentences = []
-
-        for i in range(0, len(tokens), max_length):
-            sentences.append(' '.join(tokens[i:i + max_length]))
-
-        return sentences
+    def get_prediction(self, text):
         
+        def bio_tagging(sentence, tokens):
+            # Tokenize the sentence by spaces
+            words = sentence.split()
+
+            # Create a list of BIO tags initialized as 'O' (outside any entity)
+            bio_tags = ['O'] * len(words)
+
+            # Iterate over the tokens to assign BIO tags
+            for token in tokens:
+                entity_group = token['entity_group']
+                word = token['word']
+                start = token['start']
+                end = token['end']
+                
+                # Get the substring of the sentence from start to end to match with the token word
+                token_str = sentence[start:end]
+                
+                # Split the sentence into words and get the indexes of words that match the token string
+                token_words = token_str.split()
+
+                # Find the position of the token_words in the sentence
+                index = 0
+                for i, word in enumerate(words):
+                    if word == token_words[index]:
+                        # Found the start of the token, now mark it as B-<entity>
+                        if index == 0:
+                            bio_tags[i] = f'B-{entity_group}'
+                        else:
+                            bio_tags[i] = f'I-{entity_group}'
+                        index += 1
+                        
+                        # If all words of the token are processed, break the loop
+                        if index == len(token_words):
+                            break
+
+            return bio_tags
         
-    def get_prediction(self, text, max_length=512):
-        # split sentence (> 512)
-        sentences_split = self.split_text(text, max_length)
-
-        for sentence_splited in sentences_split:
-            # make a sentence
-            sentence = Sentence(sentence_splited)
-
-            # predict NER tags
-            self.tagger.predict(sentence)
-
+        def list_entity_scores(entities_dict):
             scores = []
+            for entry in entities_dict:
+                scores.append(entry['score'])
+            return scores
 
-            # transfer entity labels to token level
-            for entity in sentence.get_spans('ner'):
-                prefix = 'B-'
-                for token in entity:
-                    token.set_label('ner-bio', prefix + entity.tag, entity.score)
-                    prefix = 'I-'
-                    scores.append(entity.score)
+        entities_dict = self.tagger(text)
+        bio_tags = bio_tagging(text, entities_dict)
+        scores = list_entity_scores(entities_dict)
 
-            # now go through all tokens and print label
-            bio_tokens = []
-            for token in sentence:
-                try:
-                    #print(token.text, token.tag)
-                    bio_tokens.append(token.tag)
-                except:
-                    #print(token.text, "O")
-                    bio_tokens.append("O")
-                    
-        return {"entities": bio_tokens, "scores": scores}
+        """
+        print('Esse eh o TOKENS')
+        print(entities_dict)
+        
+        print('Esse eh o TEXT')
+        print(text)
+
+        print('Esse eh o BIO_TAGS')
+        print(bio_tags)
+
+        print('Esse eh o SCORES')
+        print(scores)
+        """
+
+        return {"entities": bio_tags, "scores": scores}
